bcde889016fbef9aa4e76fae582233f779fd9dee
陈晟豪 <sunghao_chan@qq.com>
2024-08-20 15:34:13 +0800

    style: 新增d.ts声明

---
+++ types/element-plus.d.ts
@@ -0,0 +1,7 @@
+export {};
+declare global {
+	const ElMessage: (typeof import('element-plus/es'))['ElMessage'];
+	const ElMessageBox: (typeof import('element-plus/es'))['ElMessageBox'];
+}
+
+// 详见https://blog.csdn.net/weixin_59916662/article/details/127334196
